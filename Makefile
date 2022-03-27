fc86ec306bb6f3e43e5edf01353a2590d70fad5f
shoutyman <shoutywan@gmail.com>
2022-03-27 17:39:00 -0500

    Created Makefile
    added functionality to the Makefile

---
+++ Makefile
@@ -0,0 +1,8 @@
+mod-v6: structures.h
+	g++ -o mod-v6 mod-v6.c
+	
+
+all: mod-v6
+
+clean:
+	rm mod-v6
--- mod-v6.c
+++ mod-v6.c
@@ -1,6 +1,6 @@
 #include <stdio.h>    
 #include <stdbool.h>
-#include "./structures.h" //contains the definitions for superblock, i-node, directory, etc.
+#include "structures.h" //contains the definitions for superblock, i-node, directory, etc.
 
 /*
 * TODO: initfs()
