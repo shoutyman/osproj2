dd98d7458f7e319416cb148c5baa473e0c95a966
leoperez55 <89830131+leoperez55@users.noreply.github.com>
2022-03-27 17:13:18 -0500

    Update mod-v6.c
    Added comments for struct of inode

--- mod-v6.c
+++ mod-v6.c
@@ -38,15 +38,19 @@ void get_free_block(); // Method we need to modify
 
 // i-node Structure
 typedef struct inode{
-    unsigned short flags;
-    unsigned short nlinks;
-    unsigned int uid;
-    unsigned int gid;
-    unsigned int size0;
-    unsigned int size1;
-    unsigned int addr[9];
-    unsigned int actime;
-    unsigned int modtime;
+    unsigned short flags;   
+    unsigned short nlinks;  //Number of links to file
+    unsigned int uid;     // user ID of Owner
+    unsigned int gid;     //group ID owner
+    unsigned int size0;   //high byte of 24-bit size
+    unsigned int size1;   //low word of 24-bit size
+    unsigned int addr[9]; //block number or device number
+    unsigned int actime;  //Time of last access
+    unsigned int modtime; //Time of last modification
+
 } inode_type; //64 Bytes in size
-typedef struct { unsigned int inode; char filename[28];
+
+typedef struct { 
+    unsigned int inode; 
+    char filename[28];
 } dir_type;//32 Bytes long
