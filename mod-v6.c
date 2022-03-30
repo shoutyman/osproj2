291b2e598f2b0f6738aa45c798b8dd095211a78c
leoperez55 <89830131+leoperez55@users.noreply.github.com>
2022-03-30 17:18:25 -0500

    Test
    New main function for getting user input

--- mod-v6.c
+++ mod-v6.c
@@ -1,11 +1,16 @@
-#include <stdio.h>    
-#include <stdbool.h>
-#include <unistd.h> //includes system calls for reading/writing files
-#include <fcntl.h>  //includes constants useful for manipulating files 
-#include <assert.h> //TESTING: allows use of assert() macro
-#include "./structures.h" //contains the definitions for superblock, i-node, directory, etc.
+#include <stdio.h>
+#include <unistd.h>    //includes system calls for reading/writing files
+#include <sys/types.h>
+#include <signal.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/stat.h>
 #include <sys/types.h> // required for lseek 
-#include <fcntl.h>
+#include <fcntl.h>     //includes constants useful for manipulating files 
+#include <assert.h>    //TESTING: allows use of assert() macro
+#include "./structures.h" //contains the definitions for superblock, i-node, directory, etc.
+#include <stdbool.h>
+
 
 /*
 * TODO: initfs()
@@ -27,8 +32,10 @@ int initfs(const char* filename = "my_v6", int fsize = 10, int isize = 2) {
             new_superblock.nfree = 200;
         }
         //populate the free[] array
-        for (int counter = 0; counter < new_superblock.nfree; counter++) {
-            new_superblock.free[counter] = counter;
+        int firstFreeBlock = isize + 1;
+
+        for (int counter = firstFreeBlock; counter < new_superblock.nfree; counter++) {
+            new_superblock.free[counter] = counter + firstFreeBlock;
         }
 
         //CREATE THE FILESYSTEM
@@ -72,9 +79,61 @@ superblock_type get_superblock(int fd) {
 }
 
 void update_superblock(superblock_type newsuper) {
-
+    lseek(fd, BLOCK_SIZE, SEEK_SET);
+    assert(fd != 0 && fd != 1 && fd != 2);
+    write(fd, &newsuper, BLOCK_SIZE);
 }
 
+
+
+
+
+
+//New main function for getting user input and storing file Name, number of data blocks and number of I-nodes
+
+int main() {
+    printf("\n You have Accessed the create File System... \n");
+    printf("To create a file system enter: EX. <initfs CDrive 400 20> \n");
+
+
+    // Sets a limit to user input command length 
+    char userInput[100];
+
+    while (1) { // Will run the system until user enters "q"
+        
+        printf("Enter Command : ");
+        // get user input
+        scanf(" %[^\n]s", userInput);
+
+        // if user presses q, exits the system.
+        if (strcmp(userInput, "q") == 0) {
+            exit(0);
+        }
+
+        char *userCommand; // function to execute "Should be initfs"
+        userCommand = strtok(userInput, " ");  //Tokenizes the user string input. I.e. every word in the userInput turns into a seperate token
+
+        // if Command is Initfs intializes file system 
+        if (strcmp(userCommand, "initfs") == 0) {          //Checks if the first word is 'Initfs'
+            char *fileName = strtok(NULL, " ");            //Points to the 2nd word which should be the file Name
+            char *dataBlocks = strtok(NULL, " ");          //Points to the 3rd word which should be the the number of data blocks (stores the number as a character)
+            char *inodes = strtok(NULL, " ");              //Points to the 4th word which should be the number of inodes   (stores the number as a character)
+           
+           //Converts the characters of data block and Inodes into integers to store them as numbers
+            int numDataBlocks = atoi(dataBlocks);  
+            int numInodes = atoi(inodes);
+            
+          //Prints user input to screen to test if everything was captured correctly  
+          printf("File name entered: %s\n", fileName );
+          printf("Number of data blocks entered: %d\n", numDataBlocks);
+          printf("Number of I-node blocks entered: %d\n", numInodes);
+            
+            }
+      }
+} //End of main
+    
+
+/*
 int main()
 { 
     //initialize the filesystem
@@ -101,6 +160,9 @@ int main()
     }
     return 0;
 }
+
+
+*/
 
 void add_free_block(int); // Method we need to modify
 void get_free_block(); // Method we need to modify
